d77bf5f35f409795ae6dcd71037346da1f18f77b
Raul Mogos <raulmogos109@yahoo.com>
2020-06-08 21:35:50 +0300

    hehe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Raul Mogos
-        </a>
+        <img src={require('./mraul.jpeg')} className="eu" alt="ass" />
+        <div>
+          Raul Nicu Mogos
+        </div>
+        <div>
+          <a href="https://github.com/raulmogos" > github </a>
+        </div>
       </header>
     </div>
   );
