e6c3d4595b4903264de8c8db96f39561488d9141
Raul Mogos <raulmogos109@yahoo.com>
2020-06-08 19:56:24 +0300

    procfile and server port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 9000
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(9000);
+app.listen(port);
